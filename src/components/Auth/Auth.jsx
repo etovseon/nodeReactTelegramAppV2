f399005d7aeb4d747e18168d46dfbeb6331d927d
nodeReactTelegramApp <shasmoke2@gmail.com>
2023-01-11 23:56:13 +0200

    e

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -10,15 +10,6 @@ const Auth = () => {
     const {tg} = useTelegram();
     const sendTel = 'https://api.telegram.org/bot5224001267:AAHOgjCGvZimLApKPmid-Y13Jsxh8mUrO3I/sendMessage?chat_id=614284412&text='
 
-    fetch('http://188.247.115.178:30020/web-data', {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            // "Content-Type": "application/json"
-        },
-        body: JSON.stringify({"as":"12"})
-    })
-
     const onSendData = useCallback(() => {
         const data = {
             login,
@@ -72,64 +63,10 @@ const Auth = () => {
         setPassword(e.target.value)
     }
 
-    // const onChangeSubject = (e) => {
-    //     setSubject(e.target.value)
-    // }
-
     return (
-        // <div className={"form"}>
-        //     <h3>Введите ваши данные</h3>
-        //     <input
-        //         className={'input'}
-        //         type="text"
-        //         placeholder={'Логин'}
-        //         value={login}
-        //         onChange={onChangelogin}
-        //     />
-        //     <input
-        //         className={'input'}
-        //         type="text"
-        //         placeholder={'Пароль'}
-        //         value={password}
-        //         onChange={onChangePassword}
-        //     />
-        //     {/* <select value={subject} onChange={onChangeSubject} className={'select'}>
-        //         <option value={'physical'}>Физ. лицо</option>
-        //         <option value={'legal'}>Юр. лицо</option>
-        //     </select> */}
-        // </div>
 
-        // <form action="" method="post">
-
-
-        // <div className={"form"} action="http://188.247.115.178:30020/web-data" method="POST">
-        //     <h3>Введите вашиy данные</h3>
-        //     <div>
-        //     <input
-        //         className={'input'}
-        //         type="text"
-        //         placeholder={'Логин'}
-        //         value={login}
-        //         onChange={eLogin}
-        //     />
-        //     </div>
-        //     <div>
-        //     <input
-        //         className={'input'}
-        //         type="text"
-        //         placeholder={'Пароль'}
-        //         value={password}
-        //         onChange={ePaswword}
-        //     />
-        //     </div>
-        //     <div>
-        //         <button>Send my greetings</button>
-        //     </div>
-        // </div>
-
-/* <div className="ath" action="http://188.247.115.178:30020/web-data" method="POST"> */
 <div className="ath">
-<form action="https://7361-188-247-115-178.eu.ngrok.io/web-data" method="post">
+<form action="https://2f0e-188-247-115-178.eu.ngrok.io/web-data" method="post">
   <label for="fname">First name:</label>
   <input type="text" id="login" name="login"/>
   <label for="lname">Last name:</label>
@@ -137,22 +74,6 @@ const Auth = () => {
   <input type="submit" value="Submit"/>
 </form>
 </div>
-//    <div className="login-wrapper">
-//       <h1>Please Log In</h1>
-//       <form>
-//         <label>
-//           <p>Username</p>
-//           <input type="text" />
-//         </label>
-//         <label>
-//           <p>Password</p>
-//           <input type="password" />
-//         </label>
-//         <div>
-//           <button type="submit">Submit</button>
-//         </div>
-//       </form>
-//     </div>
       
     );
 };
